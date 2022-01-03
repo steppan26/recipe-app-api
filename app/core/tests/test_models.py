b2479ef466b71632db256ff57c51414112053a02
Stephane Baroux <stephane.baroux@gmail.com>
2022-01-03 14:31:47 +0100

    created functions to create a new user and normalize email

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.contrib.auth import get_user, get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@stephanebaroux.com'
+        password = 'Password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """Test that the user's email is normalized"""
+        email='steph@GMAIL.COM'
+        user = get_user_model().objects.create_user(
+            email,
+            password='Pasword123'
+        )
+
+        self.assertEqual(user.email, email.lower())
